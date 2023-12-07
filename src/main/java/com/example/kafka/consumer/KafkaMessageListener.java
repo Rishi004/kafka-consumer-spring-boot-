b08fe206aa3c35357385875e57f80e31d0da07e3
Rishi004 <rishireiko@gmail.com>
2023-12-07 16:49:13 +0530

    consumer from selected partition

--- src/main/java/com/example/kafka/consumer/KafkaMessageListener.java
+++ src/main/java/com/example/kafka/consumer/KafkaMessageListener.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 import com.example.kafka.dto.EmployeeDto;
@@ -21,14 +22,15 @@ public class KafkaMessageListener {
 		this.topicNames = topicNames;
 	}
 
-//	@KafkaListener(topics = "test-topic4", groupId = "topic-group-test")
-//	public void consume(String message) {
-//		log.info("consumer consume the message -> {}", message);
-//	}
-
-	@KafkaListener(topics = "test-topic4", groupId = "topic-group-test")
-	public void consume(EmployeeDto employeeDto) {
-		log.info("consumer consume the message -> {}", employeeDto.toString());
+	@KafkaListener(topics = "second-topic", groupId = "topic-group-test2", topicPartitions = {
+			@TopicPartition(topic = "second-topic", partitions = { "2" }) })
+	public void consume(String message) {
+		log.info("consumer consume the message -> {}", message);
 	}
+
+//	@KafkaListener(topics = "test-topic4", groupId = "topic-group-test")
+//	public void consume(EmployeeDto employeeDto) {
+//		log.info("consumer consume the message -> {}", employeeDto.toString());
+//	}
 
 }
